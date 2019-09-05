ffa6498b659716b8c1d0036bff848519233875db
Wilian <wilianzilv@gmail.com>
2019-09-04 21:19:33 -0300

    release

---
+++ precache-manifest.bc5d9d54339eda3dae7249c3297475e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c543ca3f51ffd20fc4e920b5a0cd1993",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84546503f32ddd8a5813",
+    "url": "/static/js/2.858df400.chunk.js"
+  },
+  {
+    "revision": "5ebc3ad31853319f7e19",
+    "url": "/static/js/main.6341748e.chunk.js"
+  },
+  {
+    "revision": "2265ccb6c043e76ca2a6",
+    "url": "/static/js/runtime~main.18981bd9.js"
+  },
+  {
+    "revision": "cbe766297dc6143734502e1e0e5d2222",
+    "url": "/static/media/angoole-logo.cbe76629.png"
+  },
+  {
+    "revision": "3a18aefb61de5f5e167560af04fb6413",
+    "url": "/static/media/me-eating.3a18aefb.jpg"
+  },
+  {
+    "revision": "334c26d02e8df1970aa672f3a35f8b7b",
+    "url": "/static/media/raffa-moreira-logo.334c26d0.png"
+  }
+]);
